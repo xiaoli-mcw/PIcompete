77cea4bb4a49ae63390a2ba763ecf7e2db76d5e8
Xiao Li <xiaoli@mcw.edu>
2020-04-17 14:45:41 -0500

    document samp.data

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' Example data for demo
+#'
+#' @source simulation
+#' @format a data frame with columns:
+#' \describe{
+#'   \item{C}{Prevalence indicator: 1 = prevalence; 0 = no prevalence}
+#'   \item{L}{Left time point}
+#'   \item{R}{}
+#'   \item{HR_status}{}
+#'   \item{risk_score}{}
+#'   \item{event}{}
+#'   \item{L1}{}
+#'   \item{R1}{}
+#'   \item{L2}{}
+#'   \item{R2}{}
+#'   \item{strata}{}
+#'   \item{samp.wgt}{}
+#'   \item{s.frc}{}
+#'   \item{strata.frac}{}
+#' }
+#' @examples
+#' \dontrun{
+#'  samp.data
+#' }
+"samp.data"
